95fb46838d30b34687ea9d7666691c2e1a8d00ed
Shalvin shaji <shalvinshaji283@gmail.com>
2023-08-04 02:41:31 +0530

    added carousel controls for small devices

--- src/components/Homecarousel.jsx
+++ src/components/Homecarousel.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import '../css/Homecarousel.css'
 
 export default function Homecarousel() {
   return (
@@ -7,15 +8,16 @@ export default function Homecarousel() {
         id="carouselExampleInterval"
         className="carousel slide"
         data-bs-ride="carousel"
+        data-bs-touch="true"
       >
         <div className="carousel-inner">
-          <div className="carousel-item active" data-bs-interval="3000">
+          <div className="carousel-item active" data-bs-interval="2000">
             <img src="https://images.unsplash.com/photo-1689878210188-da9ac2da32d2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDEyfGJvOGpRS1RhRTBZfHxlbnwwfHx8fHw%3D&auto=format&fit=crop&w=500&q=60" className="d-block w-100" alt="..." />
           </div>
-          <div className="carousel-item" data-bs-interval="3000">
+          <div className="carousel-item" data-bs-interval="2000">
             <img src="https://images.unsplash.com/photo-1690927666654-07c9e5d62696?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDl8Ym84alFLVGFFMFl8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60" className="d-block w-100" alt="..." />
           </div>
-          <div className="carousel-item" data-bs-interval="3000">
+          <div className="carousel-item" data-bs-interval="2000">
             <img src="https://images.unsplash.com/photo-1633976067869-1eb482034701?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDIwfGJvOGpRS1RhRTBZfHxlbnwwfHx8fHw%3D&auto=format&fit=crop&w=500&q=60" className="d-block w-100" alt="..." />
           </div>
         </div>
