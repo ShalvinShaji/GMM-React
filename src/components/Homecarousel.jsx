57fd334379f3caf1abde7ee2971f9c9545429c01
Shalvin shaji <shalvinshaji283@gmail.com>
2023-08-04 05:20:33 +0530

    carousel not auto slide error corrected

--- src/components/Homecarousel.jsx
+++ src/components/Homecarousel.jsx
@@ -2,6 +2,17 @@ import React, { useEffect } from "react";
 import "../css/Homecarousel.css";
 
 export default function Homecarousel() {
+  useEffect(() => {
+    // Initialize the carousel when the component mounts
+    const carousel = new bootstrap.Carousel(document.getElementById('carouselExampleAutoplaying'), {
+      interval: 3000, // Adjust the interval as needed
+    });
+
+    // Dispose the carousel when the component unmounts
+    return () => {
+      carousel.dispose();
+    };
+  }, []);
   return (
     <>
       <div
@@ -12,21 +23,21 @@ export default function Homecarousel() {
 
       >
         <div className="carousel-inner">
-          <div className="carousel-item active" data-bs-interval="2000">
+          <div className="carousel-item active">
             <img
               src="https://images.unsplash.com/photo-1690743300187-51d68146adf7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"
               className="d-block w-100"
               alt="..."
             />
           </div>
-          <div className="carousel-item" data-bs-interval="2000">
+          <div className="carousel-item" >
             <img
               src="https://images.unsplash.com/photo-1690460814053-5906ac3e90cd?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDQ5fGJvOGpRS1RhRTBZfHxlbnwwfHx8fHw%3D&auto=format&fit=crop&w=500&q=60"
               className="d-block w-100"
               alt="..."
             />
           </div>
-          <div className="carousel-item" data-bs-interval="2000">
+          <div className="carousel-item">
             <img
               src="https://images.unsplash.com/photo-1690394472903-23ce57cb5953?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDUzfGJvOGpRS1RhRTBZfHxlbnwwfHx8fHw%3D&auto=format&fit=crop&w=500&q=60"
               className="d-block w-100"
