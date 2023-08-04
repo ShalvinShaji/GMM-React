12278f32ef5c1d919510504b22ae66b39a69033f
Shalvin shaji <shalvinshaji283@gmail.com>
2023-08-04 21:39:06 +0530

    carousel images time incresed to 7 sec

--- src/components/Homecarousel.jsx
+++ src/components/Homecarousel.jsx
@@ -18,25 +18,25 @@ export default function Homecarousel() {
         className="carousel slide carousel-fade"
         data-bs-ride="carousel"
         data-bs-pause="false"
-        data-bs-interval="3000"
+        
 
       >
         <div className="carousel-inner">
-          <div className="carousel-item active">
+          <div className="carousel-item active" data-bs-interval="6000">
             <img
               src="https://images.hdqwalls.com/wallpapers/i-am-iron-man-marvels-avengers-62.jpg"
               className="d-block w-100"
               alt="..."
             />
           </div>
-          <div className="carousel-item" >
+          <div className="carousel-item" data-bs-interval="6000">
             <img
               src="https://images.hdqwalls.com/wallpapers/hulk-marvels-avengers-40.jpg"
               className="d-block w-100"
               alt="..."
             />
           </div>
-          <div className="carousel-item">
+          <div className="carousel-item" data-bs-interval="6000">
             <img
               src="https://images.hdqwalls.com/wallpapers/love-never-ends-wallpaper.jpg"
               className="d-block w-100"
