5234771530afb58ec9674740ceb7ff0e980be497
Shalvin shaji <shalvinshaji283@gmail.com>
2023-08-04 21:34:34 +0530

    carousel images changed

--- src/components/Homecarousel.jsx
+++ src/components/Homecarousel.jsx
@@ -4,9 +4,7 @@ import "../css/Homecarousel.css";
 export default function Homecarousel() {
   useEffect(() => {
     // Initialize the carousel when the component mounts
-    const carousel = new bootstrap.Carousel(document.getElementById('carouselExampleAutoplaying'), {
-      interval: 3000, // Adjust the interval as needed
-    });
+    const carousel = new bootstrap.Carousel(document.getElementById('carouselExampleAutoplaying'));
 
     // Dispose the carousel when the component unmounts
     return () => {
@@ -20,26 +18,27 @@ export default function Homecarousel() {
         className="carousel slide carousel-fade"
         data-bs-ride="carousel"
         data-bs-pause="false"
+        data-bs-interval="3000"
 
       >
         <div className="carousel-inner">
           <div className="carousel-item active">
             <img
-              src="https://images.unsplash.com/photo-1690743300187-51d68146adf7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"
+              src="https://images.hdqwalls.com/wallpapers/i-am-iron-man-marvels-avengers-62.jpg"
               className="d-block w-100"
               alt="..."
             />
           </div>
           <div className="carousel-item" >
             <img
-              src="https://images.unsplash.com/photo-1690460814053-5906ac3e90cd?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDQ5fGJvOGpRS1RhRTBZfHxlbnwwfHx8fHw%3D&auto=format&fit=crop&w=500&q=60"
+              src="https://images.hdqwalls.com/wallpapers/hulk-marvels-avengers-40.jpg"
               className="d-block w-100"
               alt="..."
             />
           </div>
           <div className="carousel-item">
             <img
-              src="https://images.unsplash.com/photo-1690394472903-23ce57cb5953?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDUzfGJvOGpRS1RhRTBZfHxlbnwwfHx8fHw%3D&auto=format&fit=crop&w=500&q=60"
+              src="https://images.hdqwalls.com/wallpapers/love-never-ends-wallpaper.jpg"
               className="d-block w-100"
               alt="..."
             />
