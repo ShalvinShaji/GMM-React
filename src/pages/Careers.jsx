a83e85dd3d36a884c018cfe69155683c98dce7d5
Shalvin shaji <shalvinshaji283@gmail.com>
2023-08-28 03:33:27 +0530

    gallery page completed

--- src/pages/Careers.jsx
+++ src/pages/Careers.jsx
@@ -8,21 +8,25 @@ export default function Careers() {
     <>
       <Bcrumbs breadcrumbspage="Careers" />
       <section className="Careers" id="Careers">
-        <Sectionhead sectionname="Careers"/>
+        <Sectionhead sectionname="Careers" />
         <div className="container">
-        <div className="row">
-                <div className="col-lg-4 col-md-6 g-2 p-3 m-0">
-                    <div className="careers-section">
-                        <div className="careers-banner p-2">
-                            <img src="https://images.hdqwalls.com/wallpapers/mortal-kombat-mobile-logo-5o.jpg" alt="careers-banner" srcSet=""/>
-                        </div>
-                        <div className="careers-description p-2">
-                            <div className="careers-title mb-3">
-                                <h4 className="text-capitalize">
-                                    Consultant General Medicine
-                                </h4>
-                            </div>
-                            {/* <div className="careers-qualification">
+          <div className="row">
+            <div className="col-lg-4 col-md-6 g-2 p-3 m-0">
+              <div className="careers-section">
+                <div className="careers-banner p-2">
+                  <img
+                    src="https://images.hdqwalls.com/wallpapers/mortal-kombat-mobile-logo-5o.jpg"
+                    alt="careers-banner"
+                    className="image-fluid"
+                  />
+                </div>
+                <div className="careers-description p-2">
+                  <div className="careers-title mb-3">
+                    <h4 className="text-capitalize">
+                      Consultant General Medicine
+                    </h4>
+                  </div>
+                  {/* <div className="careers-qualification">
                                 <h5 className="p-0 m-0 text-capitalize">Qualification<span>:</span></h5>
                                 <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
                             </div>
@@ -31,139 +35,169 @@ export default function Careers() {
                                 <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Lorem ipsum dolor sit amet
                                     consectetur adipisicing elit. Hic rerum voluptatum alias quam qui laboriosam?</p>
                             </div> */}
-                            <div className="careers-contact">
-                                <p>Please send your resume to <a href="mailto:gmmhospital@gmail.com"
-                                        className="gmm-email">gmmhospital@gmail.com</a></p>
-                            </div>
-
-                        </div>
-
-                    </div>
-
+                  <div className="careers-contact">
+                    <p>
+                      Please send your resume to{" "}
+                      <a
+                        href="mailto:gmmhospital@gmail.com"
+                        className="gmm-email"
+                      >
+                        gmmhospital@gmail.com
+                      </a>
+                    </p>
+                  </div>
                 </div>
-                <div className="col-lg-4 col-md-6 g-2 p-3 m-0">
-                    <div className="careers-section">
-                        <div className="careers-banner p-2">
-                            <img src="https://images.hdqwalls.com/wallpapers/mortal-kombat-mobile-logo-5o.jpg" alt="careers-banner" srcSet=""/>
-                        </div>
-                        <div className="careers-description p-2">
-                            <div className="careers-title mb-3">
-                                <h4 className="text-capitalize">
-                                    Consultant General Medicine
-                                </h4>
-                            </div>
-                            {/* <div className="careers-qualification">
-                                <h5 className="p-0 m-0 text-capitalize">Qualification<span>:</span></h5>
-                                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
-                            </div>
-                            <div className="careers-experience">
-                                <h5 className="p-0 m-0 text-capitalize">Experience<span>:</span></h5>
-                                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Lorem ipsum dolor sit amet
-                                    consectetur adipisicing elit. Hic rerum voluptatum alias quam qui laboriosam?</p>
-                            </div> */}
-                            <div className="careers-contact">
-                                <p>Please send your resume to <a href="mailto:gmmhospital@gmail.com"
-                                        className="gmm-email">gmmhospital@gmail.com</a></p>
-                            </div>
-
-                        </div>
-
-                    </div>
-
-                </div>
-                <div className="col-lg-4 col-md-6 g-2 p-3 m-0">
-                    <div className="careers-section">
-                        <div className="careers-banner p-2">
-                            <img src="https://images.hdqwalls.com/wallpapers/mortal-kombat-mobile-logo-5o.jpg" alt="careers-banner" srcSet=""/>
-                        </div>
-                        <div className="careers-description p-2">
-                            <div className="careers-title mb-3">
-                                <h4 className="text-capitalize">
-                                    Consultant General Medicine
-                                </h4>
-                            </div>
-                            {/* <div className="careers-qualification">
-                                <h5 className="p-0 m-0 text-capitalize">Qualification<span>:</span></h5>
-                                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
-                            </div>
-                            <div className="careers-experience">
-                                <h5 className="p-0 m-0 text-capitalize">Experience<span>:</span></h5>
-                                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Lorem ipsum dolor sit amet
-                                    consectetur adipisicing elit. Hic rerum voluptatum alias quam qui laboriosam?</p>
-                            </div> */}
-                            <div className="careers-contact">
-                                <p>Please send your resume to <a href="mailto:gmmhospital@gmail.com"
-                                        className="gmm-email">gmmhospital@gmail.com</a></p>
-                            </div>
-
-                        </div>
-
-                    </div>
-
-                </div>
-                <div className="col-lg-4 col-md-6 g-2 p-3 m-0">
-                    <div className="careers-section">
-                        <div className="careers-banner p-2">
-                            <img src="https://images.hdqwalls.com/wallpapers/mortal-kombat-mobile-logo-5o.jpg" alt="careers-banner" srcSet=""/>
-                        </div>
-                        <div className="careers-description p-2">
-                            <div className="careers-title mb-3">
-                                <h4 className="text-capitalize">
-                                    Consultant General Medicine
-                                </h4>
-                            </div>
-                            {/* <div className="careers-qualification">
-                                <h5 className="p-0 m-0 text-capitalize">Qualification<span>:</span></h5>
-                                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
-                            </div>
-                            <div className="careers-experience">
-                                <h5 className="p-0 m-0 text-capitalize">Experience<span>:</span></h5>
-                                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Lorem ipsum dolor sit amet
-                                    consectetur adipisicing elit. Hic rerum voluptatum alias quam qui laboriosam?</p>
-                            </div> */}
-                            <div className="careers-contact">
-                                <p>Please send your resume to <a href="mailto:gmmhospital@gmail.com"
-                                        className="gmm-email">gmmhospital@gmail.com</a></p>
-                            </div>
-
-                        </div>
-
-                    </div>
-
-                </div>
-                <div className="col-lg-4 col-md-6 g-2 p-3 m-0">
-                    <div className="careers-section">
-                        <div className="careers-banner p-2">
-                            <img src="https://images.hdqwalls.com/wallpapers/mortal-kombat-mobile-logo-5o.jpg" alt="careers-banner" srcSet=""/>
-                        </div>
-                        <div className="careers-description p-2">
-                            <div className="careers-title mb-3">
-                                <h4 className="text-capitalize">
-                                    Consultant General Medicine
-                                </h4>
-                            </div>
-                            {/* <div className="careers-qualification">
-                                <h5 className="p-0 m-0 text-capitalize">Qualification<span>:</span></h5>
-                                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
-                            </div>
-                            <div className="careers-experience">
-                                <h5 className="p-0 m-0 text-capitalize">Experience<span>:</span></h5>
-                                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Lorem ipsum dolor sit amet
-                                    consectetur adipisicing elit. Hic rerum voluptatum alias quam qui laboriosam?</p>
-                            </div> */}
-                            <div className="careers-contact">
-                                <p>Please send your resume to <a href="mailto:gmmhospital@gmail.com"
-                                        className="gmm-email">gmmhospital@gmail.com</a></p>
-                            </div>
-
-                        </div>
-
-                    </div>
-
-                </div>
-                
+              </div>
             </div>
-
+            <div className="col-lg-4 col-md-6 g-2 p-3 m-0">
+              <div className="careers-section">
+                <div className="careers-banner p-2">
+                  <img src="https://images.hdqwalls.com/wallpapers/mortal-kombat-mobile-logo-5o.jpg" />
+                </div>
+                <div className="careers-description p-2">
+                  <div className="careers-title mb-3">
+                    <h4 className="text-capitalize">
+                      Consultant General Medicine
+                    </h4>
+                  </div>
+                  {/* <div className="careers-qualification">
+                                <h5 className="p-0 m-0 text-capitalize">Qualification<span>:</span></h5>
+                                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+                            </div>
+                            <div className="careers-experience">
+                                <h5 className="p-0 m-0 text-capitalize">Experience<span>:</span></h5>
+                                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Lorem ipsum dolor sit amet
+                                    consectetur adipisicing elit. Hic rerum voluptatum alias quam qui laboriosam?</p>
+                            </div> */}
+                  <div className="careers-contact">
+                    <p>
+                      Please send your resume to{" "}
+                      <a
+                        href="mailto:gmmhospital@gmail.com"
+                        className="gmm-email"
+                      >
+                        gmmhospital@gmail.com
+                      </a>
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-4 col-md-6 g-2 p-3 m-0">
+              <div className="careers-section">
+                <div className="careers-banner p-2">
+                  <img
+                    src="https://images.hdqwalls.com/wallpapers/mortal-kombat-mobile-logo-5o.jpg"
+                    alt="careers-banner"
+                    className="image-fluid"
+                  />
+                </div>
+                <div className="careers-description p-2">
+                  <div className="careers-title mb-3">
+                    <h4 className="text-capitalize">
+                      Consultant General Medicine
+                    </h4>
+                  </div>
+                  {/* <div className="careers-qualification">
+                                <h5 className="p-0 m-0 text-capitalize">Qualification<span>:</span></h5>
+                                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+                            </div>
+                            <div className="careers-experience">
+                                <h5 className="p-0 m-0 text-capitalize">Experience<span>:</span></h5>
+                                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Lorem ipsum dolor sit amet
+                                    consectetur adipisicing elit. Hic rerum voluptatum alias quam qui laboriosam?</p>
+                            </div> */}
+                  <div className="careers-contact">
+                    <p>
+                      Please send your resume to{" "}
+                      <a
+                        href="mailto:gmmhospital@gmail.com"
+                        className="gmm-email"
+                      >
+                        gmmhospital@gmail.com
+                      </a>
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-4 col-md-6 g-2 p-3 m-0">
+              <div className="careers-section">
+                <div className="careers-banner p-2">
+                  <img
+                    src="https://images.hdqwalls.com/wallpapers/mortal-kombat-mobile-logo-5o.jpg"
+                    alt="careers-banner"
+                    className="image-fluid"
+                  />
+                </div>
+                <div className="careers-description p-2">
+                  <div className="careers-title mb-3">
+                    <h4 className="text-capitalize">
+                      Consultant General Medicine
+                    </h4>
+                  </div>
+                  {/* <div className="careers-qualification">
+                                <h5 className="p-0 m-0 text-capitalize">Qualification<span>:</span></h5>
+                                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+                            </div>
+                            <div className="careers-experience">
+                                <h5 className="p-0 m-0 text-capitalize">Experience<span>:</span></h5>
+                                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Lorem ipsum dolor sit amet
+                                    consectetur adipisicing elit. Hic rerum voluptatum alias quam qui laboriosam?</p>
+                            </div> */}
+                  <div className="careers-contact">
+                    <p>
+                      Please send your resume to{" "}
+                      <a
+                        href="mailto:gmmhospital@gmail.com"
+                        className="gmm-email"
+                      >
+                        gmmhospital@gmail.com
+                      </a>
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-4 col-md-6 g-2 p-3 m-0">
+              <div className="careers-section">
+                <div className="careers-banner p-2">
+                  <img
+                    src="https://images.hdqwalls.com/wallpapers/mortal-kombat-mobile-logo-5o.jpg"
+                    alt="careers-banner"
+                    className="image-fluid"
+                  />
+                </div>
+                <div className="careers-description p-2">
+                  <div className="careers-title mb-3">
+                    <h4 className="text-capitalize">
+                      Consultant General Medicine
+                    </h4>
+                  </div>
+                  {/* <div className="careers-qualification">
+                                <h5 className="p-0 m-0 text-capitalize">Qualification<span>:</span></h5>
+                                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+                            </div>
+                            <div className="careers-experience">
+                                <h5 className="p-0 m-0 text-capitalize">Experience<span>:</span></h5>
+                                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Lorem ipsum dolor sit amet
+                                    consectetur adipisicing elit. Hic rerum voluptatum alias quam qui laboriosam?</p>
+                            </div> */}
+                  <div className="careers-contact">
+                    <p>
+                      Please send your resume to{" "}
+                      <a
+                        href="mailto:gmmhospital@gmail.com"
+                        className="gmm-email"
+                      >
+                        gmmhospital@gmail.com
+                      </a>
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </section>
     </>
