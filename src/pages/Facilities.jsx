80fd720af34a4ec25b838b27a2c8b27104527590
Shalvin shaji <shalvinshaji283@gmail.com>
2023-10-10 02:13:03 +0530

    facility page details added using api mapping

--- src/pages/Facilities.jsx
+++ src/pages/Facilities.jsx
@@ -4,6 +4,34 @@ import Sectionhead from "../components/Sectionhead";
 import "../css/Facilities.css";
 
 export default function Facilities() {
+  const facilitiesData = [
+    {
+      id: 1,
+      name: "Casualty",
+      image:
+        "https://www.onlinelogomaker.com/blog/wp-content/uploads/2017/07/medical-logo.jpg",
+    },
+    {
+      id: 2,
+      name: "Casualty",
+      image:
+        "https://www.onlinelogomaker.com/blog/wp-content/uploads/2017/07/medical-logo.jpg",
+    },
+    {
+      id: 3,
+      name: "Casualty",
+      image:
+        "https://www.onlinelogomaker.com/blog/wp-content/uploads/2017/07/medical-logo.jpg",
+    },
+    {
+      id: 4,
+      name: "Casualty",
+      image:
+        "https://www.onlinelogomaker.com/blog/wp-content/uploads/2017/07/medical-logo.jpg",
+    },
+    // Add more facility objects as needed
+  ];
+
   return (
     <>
       <Bcrumbs breadcrumbspage="Facilities" />
@@ -15,46 +43,18 @@ export default function Facilities() {
         />
         <div className="container">
           <div className="row">
-            <div className="col-lg-3">
-              <div className="facility">
-                <div className="facility-img">
-                  <img src="./assets/img/facilities/casualty.png" alt="" />
-                </div>
-                <div className="facility-name">
-                  <h3 className="text-uppercase">casualty</h3>
-                </div>
-              </div>
-            </div>
-            <div className="col-lg-3">
-              <div className="facility">
-                <div className="facility-img">
-                  <img src="./assets/img/facilities/casualty.png" alt="" />
-                </div>
-                <div className="facility-name">
-                  <h3 className="text-uppercase">casualty</h3>
+            {facilitiesData.map((facility) => (
+              <div className="col-lg-4 col-xl-3 col-md-6" key={facility.id}>
+                <div className="facility">
+                  <div className="facility-img">
+                    <img src={facility.image} alt={facility.name} />
+                  </div>
+                  <div className="facility-name">
+                    <h3 className="text-uppercase">{facility.name}</h3>
+                  </div>
                 </div>
               </div>
-            </div>
-            <div className="col-lg-3">
-              <div className="facility">
-                <div className="facility-img">
-                  <img src="./assets/img/facilities/casualty.png" alt="" />
-                </div>
-                <div className="facility-name">
-                  <h3 className="text-uppercase">casualty</h3>
-                </div>
-              </div>
-            </div>
-            <div className="col-lg-3">
-              <div className="facility">
-                <div className="facility-img">
-                  <img src="./assets/img/facilities/casualty.png" alt="" />
-                </div>
-                <div className="facility-name">
-                  <h3 className="text-uppercase">casualty</h3>
-                </div>
-              </div>
-            </div>
+            ))}
           </div>
         </div>
       </section>
