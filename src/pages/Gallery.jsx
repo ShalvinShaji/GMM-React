34a2a4e054ce30cddae7b8a0420825327e1251c1
Shalvin shaji <shalvinshaji283@gmail.com>
2023-10-10 02:02:10 +0530

    gallery page details added using api mapping

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -7,6 +7,47 @@ import Modal from "react-modal";
 export default function Gallery() {
   const [selectedImage, setSelectedImage] = useState(null);
 
+  // List of image groups
+  const imageGroups = [
+    {
+      title: "Summer medical camp 2023",
+      images: [
+        "https://images.hdqwalls.com/wallpapers/age-of-wonders-4-2023-50.jpg",
+        "https://images.hdqwalls.com/wallpapers/age-of-wonders-4-2023-50.jpg",
+        "https://images.hdqwalls.com/wallpapers/age-of-wonders-4-2023-50.jpg",
+        "https://images.hdqwalls.com/wallpapers/age-of-wonders-4-2023-50.jpg",
+        "https://images.hdqwalls.com/wallpapers/age-of-wonders-4-2023-50.jpg",
+        "https://images.hdqwalls.com/wallpapers/age-of-wonders-4-2023-50.jpg",
+        "https://images.hdqwalls.com/wallpapers/age-of-wonders-4-2023-50.jpg",
+        "https://images.hdqwalls.com/wallpapers/age-of-wonders-4-2023-50.jpg",
+        "https://images.hdqwalls.com/wallpapers/age-of-wonders-4-2023-50.jpg",
+        "https://images.hdqwalls.com/wallpapers/age-of-wonders-4-2023-50.jpg",
+        "https://images.hdqwalls.com/wallpapers/age-of-wonders-4-2023-50.jpg",
+        "https://images.hdqwalls.com/wallpapers/age-of-wonders-4-2023-50.jpg",
+        // Add more images for this group
+      ],
+    },
+    {
+      title: "Summer medical camp 2023",
+      images: [
+        "https://images.hdqwalls.com/wallpapers/age-of-wonders-4-2023-50.jpg",
+        "https://images.hdqwalls.com/wallpapers/age-of-wonders-4-2023-50.jpg",
+        "https://images.hdqwalls.com/wallpapers/age-of-wonders-4-2023-50.jpg",
+        "https://images.hdqwalls.com/wallpapers/age-of-wonders-4-2023-50.jpg",
+        "https://images.hdqwalls.com/wallpapers/age-of-wonders-4-2023-50.jpg",
+        "https://images.hdqwalls.com/wallpapers/age-of-wonders-4-2023-50.jpg",
+        "https://images.hdqwalls.com/wallpapers/age-of-wonders-4-2023-50.jpg",
+        "https://images.hdqwalls.com/wallpapers/age-of-wonders-4-2023-50.jpg",
+        "https://images.hdqwalls.com/wallpapers/age-of-wonders-4-2023-50.jpg",
+        "https://images.hdqwalls.com/wallpapers/age-of-wonders-4-2023-50.jpg",
+        "https://images.hdqwalls.com/wallpapers/age-of-wonders-4-2023-50.jpg",
+        "https://images.hdqwalls.com/wallpapers/age-of-wonders-4-2023-50.jpg",
+        // Add more images for this group
+      ],
+    },
+    // Add more image groups as needed
+  ];
+
   const openModal = (imageSrc) => {
     setSelectedImage(imageSrc);
   };
@@ -25,196 +66,29 @@ export default function Gallery() {
           sectiondescriptionbold="Gallery"
         />
         <div className="container">
-          <div className="gallery-item">
-            <div className="gallery-item">
-              <div className="gallery-item-title">
-                <h4> Summer medical camp 2023</h4>
-              </div>
-              <div className="row g-3">
-                <div
-                  className="col-6 col-md-4 col-lg-3 col-xl-2 gallery-img d-flex justify-content-center align-items-center"
-                  onClick={() =>
-                    openModal(
-                      "https://images.hdqwalls.com/wallpapers/age-of-wonders-4-2023-50.jpg"
-                    )
-                  }
-                >
-                  <img
-                    src="https://images.hdqwalls.com/wallpapers/age-of-wonders-4-2023-50.jpg"
-                    alt="sample image"
-                    className="img-fluid"
-                  />
+          <div className="row g-5">
+            {imageGroups.map((group, index) => (
+              <div key={index} className="gallery-item">
+                <div className="gallery-item-title">
+                  <h4>{group.title}</h4>
                 </div>
-                <div
-                  className="col-6 col-md-4 col-lg-3 col-xl-2 gallery-img d-flex justify-content-center align-items-center"
-                  onClick={() =>
-                    openModal(
-                      "https://images.hdqwalls.com/wallpapers/age-of-wonders-4-2023-50.jpg"
-                    )
-                  }
-                >
-                  <img
-                    src="https://images.hdqwalls.com/wallpapers/age-of-wonders-4-2023-50.jpg"
-                    alt="sample image"
-                    className="img-fluid"
-                  />
-                </div>
-                <div
-                  className="col-6 col-md-4 col-lg-3 col-xl-2 gallery-img d-flex justify-content-center align-items-center"
-                  onClick={() =>
-                    openModal(
-                      "https://images.hdqwalls.com/wallpapers/age-of-wonders-4-2023-50.jpg"
-                    )
-                  }
-                >
-                  <img
-                    src="https://images.hdqwalls.com/wallpapers/age-of-wonders-4-2023-50.jpg"
-                    alt="sample image"
-                    className="img-fluid"
-                  />
-                </div>
-                <div
-                  className="col-6 col-md-4 col-lg-3 col-xl-2 gallery-img d-flex justify-content-center align-items-center"
-                  onClick={() =>
-                    openModal(
-                      "https://images.hdqwalls.com/wallpapers/age-of-wonders-4-2023-50.jpg"
-                    )
-                  }
-                >
-                  <img
-                    src="https://images.hdqwalls.com/wallpapers/age-of-wonders-4-2023-50.jpg"
-                    alt="sample image"
-                    className="img-fluid"
-                  />
-                </div>
-                <div
-                  className="col-6 col-md-4 col-lg-3 col-xl-2 gallery-img d-flex justify-content-center align-items-center"
-                  onClick={() =>
-                    openModal(
-                      "https://images.hdqwalls.com/wallpapers/age-of-wonders-4-2023-50.jpg"
-                    )
-                  }
-                >
-                  <img
-                    src="https://images.hdqwalls.com/wallpapers/age-of-wonders-4-2023-50.jpg"
-                    alt="sample image"
-                    className="img-fluid"
-                  />
-                </div>
-                <div
-                  className="col-6 col-md-4 col-lg-3 col-xl-2 gallery-img d-flex justify-content-center align-items-center"
-                  onClick={() =>
-                    openModal(
-                      "https://images.hdqwalls.com/wallpapers/age-of-wonders-4-2023-50.jpg"
-                    )
-                  }
-                >
-                  <img
-                    src="https://images.hdqwalls.com/wallpapers/age-of-wonders-4-2023-50.jpg"
-                    alt="sample image"
-                    className="img-fluid"
-                  />
-                </div>
-                <div
-                  className="col-6 col-md-4 col-lg-3 col-xl-2 gallery-img d-flex justify-content-center align-items-center"
-                  onClick={() =>
-                    openModal(
-                      "https://images.hdqwalls.com/wallpapers/age-of-wonders-4-2023-50.jpg"
-                    )
-                  }
-                >
-                  <img
-                    src="https://images.hdqwalls.com/wallpapers/age-of-wonders-4-2023-50.jpg"
-                    alt="sample image"
-                    className="img-fluid"
-                  />
-                </div>
-                <div
-                  className="col-6 col-md-4 col-lg-3 col-xl-2 gallery-img d-flex justify-content-center align-items-center"
-                  onClick={() =>
-                    openModal(
-                      "https://images.hdqwalls.com/wallpapers/age-of-wonders-4-2023-50.jpg"
-                    )
-                  }
-                >
-                  <img
-                    src="https://images.hdqwalls.com/wallpapers/age-of-wonders-4-2023-50.jpg"
-                    alt="sample image"
-                    className="img-fluid"
-                  />
-                </div>
-                <div
-                  className="col-6 col-md-4 col-lg-3 col-xl-2 gallery-img d-flex justify-content-center align-items-center"
-                  onClick={() =>
-                    openModal(
-                      "https://images.hdqwalls.com/wallpapers/age-of-wonders-4-2023-50.jpg"
-                    )
-                  }
-                >
-                  <img
-                    src="https://images.hdqwalls.com/wallpapers/age-of-wonders-4-2023-50.jpg"
-                    alt="sample image"
-                    className="img-fluid"
-                  />
-                </div>
-                <div
-                  className="col-6 col-md-4 col-lg-3 col-xl-2 gallery-img d-flex justify-content-center align-items-center"
-                  onClick={() =>
-                    openModal(
-                      "https://images.hdqwalls.com/wallpapers/age-of-wonders-4-2023-50.jpg"
-                    )
-                  }
-                >
-                  <img
-                    src="https://images.hdqwalls.com/wallpapers/age-of-wonders-4-2023-50.jpg"
-                    alt="sample image"
-                    className="img-fluid"
-                  />
-                </div>
-                <div
-                  className="col-6 col-md-4 col-lg-3 col-xl-2 gallery-img d-flex justify-content-center align-items-center"
-                  onClick={() =>
-                    openModal(
-                      "https://images.hdqwalls.com/wallpapers/age-of-wonders-4-2023-50.jpg"
-                    )
-                  }
-                >
-                  <img
-                    src="https://images.hdqwalls.com/wallpapers/age-of-wonders-4-2023-50.jpg"
-                    alt="sample image"
-                    className="img-fluid"
-                  />
-                </div>
-                <div
-                  className="col-6 col-md-4 col-lg-3 col-xl-2 gallery-img d-flex justify-content-center align-items-center"
-                  onClick={() =>
-                    openModal(
-                      "https://images.hdqwalls.com/wallpapers/age-of-wonders-4-2023-50.jpg"
-                    )
-                  }
-                >
-                  <img
-                    src="https://images.hdqwalls.com/wallpapers/age-of-wonders-4-2023-50.jpg"
-                    alt="sample image"
-                    className="img-fluid"
-                  />
-                </div>
-                <div
-                  className="col-6 col-md-4 col-lg-3 col-xl-2 gallery-img d-flex justify-content-center align-items-center"
-                  onClick={() =>
-                    openModal(
-                      "https://images.hdqwalls.com/wallpapers/age-of-wonders-4-2023-50.jpg"
-                    )
-                  }
-                >
-                  <img
-                    src="https://images.hdqwalls.com/wallpapers/age-of-wonders-4-2023-50.jpg"
-                    alt="sample image"
-                    className="img-fluid"
-                  />
+                <div className="row g-3">
+                  {group.images.map((image, i) => (
+                    <div
+                      key={i}
+                      className="col-6 col-md-4 col-lg-3 col-xl-2 gallery-img d-flex justify-content-center align-items-center"
+                      onClick={() => openModal(image)}
+                    >
+                      <img
+                        src={image}
+                        alt={`sample image ${i + 1}`}
+                        className="img-fluid"
+                      />
+                    </div>
+                  ))}
                 </div>
               </div>
-            </div>
+            ))}
           </div>
         </div>
       </section>
