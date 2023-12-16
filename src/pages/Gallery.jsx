e2f624f90216dabbc44dae85133061750b35a351
Shalvin shaji <shalvinshaji283@gmail.com>
2023-12-16 15:21:41 +0530

    gallery  page divided into functions

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -7,46 +7,73 @@ import Modal from "react-modal";
 export default function Gallery() {
   const [selectedImage, setSelectedImage] = useState(null);
 
-  // List of image groups
-  const imageGroups = [
-    {
-      title: "Summer medical camp 2023",
-      images: [
-        "https://images.hdqwalls.com/wallpapers/age-of-wonders-4-2023-50.jpg",
-        "https://images.hdqwalls.com/wallpapers/age-of-wonders-4-2023-50.jpg",
-        "https://images.hdqwalls.com/wallpapers/age-of-wonders-4-2023-50.jpg",
-        "https://images.hdqwalls.com/wallpapers/age-of-wonders-4-2023-50.jpg",
-        "https://images.hdqwalls.com/wallpapers/age-of-wonders-4-2023-50.jpg",
-        "https://images.hdqwalls.com/wallpapers/age-of-wonders-4-2023-50.jpg",
-        "https://images.hdqwalls.com/wallpapers/age-of-wonders-4-2023-50.jpg",
-        "https://images.hdqwalls.com/wallpapers/age-of-wonders-4-2023-50.jpg",
-        "https://images.hdqwalls.com/wallpapers/age-of-wonders-4-2023-50.jpg",
-        "https://images.hdqwalls.com/wallpapers/age-of-wonders-4-2023-50.jpg",
-        "https://images.hdqwalls.com/wallpapers/age-of-wonders-4-2023-50.jpg",
-        "https://images.hdqwalls.com/wallpapers/age-of-wonders-4-2023-50.jpg",
-        // Add more images for this group
-      ],
-    },
-    {
-      title: "Summer medical camp 2023",
-      images: [
-        "https://images.hdqwalls.com/wallpapers/age-of-wonders-4-2023-50.jpg",
-        "https://images.hdqwalls.com/wallpapers/age-of-wonders-4-2023-50.jpg",
-        "https://images.hdqwalls.com/wallpapers/age-of-wonders-4-2023-50.jpg",
-        "https://images.hdqwalls.com/wallpapers/age-of-wonders-4-2023-50.jpg",
-        "https://images.hdqwalls.com/wallpapers/age-of-wonders-4-2023-50.jpg",
-        "https://images.hdqwalls.com/wallpapers/age-of-wonders-4-2023-50.jpg",
-        "https://images.hdqwalls.com/wallpapers/age-of-wonders-4-2023-50.jpg",
-        "https://images.hdqwalls.com/wallpapers/age-of-wonders-4-2023-50.jpg",
-        "https://images.hdqwalls.com/wallpapers/age-of-wonders-4-2023-50.jpg",
-        "https://images.hdqwalls.com/wallpapers/age-of-wonders-4-2023-50.jpg",
-        "https://images.hdqwalls.com/wallpapers/age-of-wonders-4-2023-50.jpg",
-        "https://images.hdqwalls.com/wallpapers/age-of-wonders-4-2023-50.jpg",
-        // Add more images for this group
-      ],
-    },
-    // Add more image groups as needed
-  ];
+  function galleryData() {
+    const imageGroups = [
+      {
+        title: "Summer medical camp 2023",
+        images: [
+          "https://images.hdqwalls.com/wallpapers/age-of-wonders-4-2023-50.jpg",
+          "https://images.hdqwalls.com/wallpapers/age-of-wonders-4-2023-50.jpg",
+          "https://images.hdqwalls.com/wallpapers/age-of-wonders-4-2023-50.jpg",
+          "https://images.hdqwalls.com/wallpapers/age-of-wonders-4-2023-50.jpg",
+          "https://images.hdqwalls.com/wallpapers/age-of-wonders-4-2023-50.jpg",
+          "https://images.hdqwalls.com/wallpapers/age-of-wonders-4-2023-50.jpg",
+          "https://images.hdqwalls.com/wallpapers/age-of-wonders-4-2023-50.jpg",
+          "https://images.hdqwalls.com/wallpapers/age-of-wonders-4-2023-50.jpg",
+          "https://images.hdqwalls.com/wallpapers/age-of-wonders-4-2023-50.jpg",
+          "https://images.hdqwalls.com/wallpapers/age-of-wonders-4-2023-50.jpg",
+          "https://images.hdqwalls.com/wallpapers/age-of-wonders-4-2023-50.jpg",
+          "https://images.hdqwalls.com/wallpapers/age-of-wonders-4-2023-50.jpg",
+          // Add more images for this group
+        ],
+      },
+      {
+        title: "Summer medical camp 2023",
+        images: [
+          "https://images.hdqwalls.com/wallpapers/age-of-wonders-4-2023-50.jpg",
+          "https://images.hdqwalls.com/wallpapers/age-of-wonders-4-2023-50.jpg",
+          "https://images.hdqwalls.com/wallpapers/age-of-wonders-4-2023-50.jpg",
+          "https://images.hdqwalls.com/wallpapers/age-of-wonders-4-2023-50.jpg",
+          "https://images.hdqwalls.com/wallpapers/age-of-wonders-4-2023-50.jpg",
+          "https://images.hdqwalls.com/wallpapers/age-of-wonders-4-2023-50.jpg",
+          "https://images.hdqwalls.com/wallpapers/age-of-wonders-4-2023-50.jpg",
+          "https://images.hdqwalls.com/wallpapers/age-of-wonders-4-2023-50.jpg",
+          "https://images.hdqwalls.com/wallpapers/age-of-wonders-4-2023-50.jpg",
+          "https://images.hdqwalls.com/wallpapers/age-of-wonders-4-2023-50.jpg",
+          "https://images.hdqwalls.com/wallpapers/age-of-wonders-4-2023-50.jpg",
+          "https://images.hdqwalls.com/wallpapers/age-of-wonders-4-2023-50.jpg",
+          // Add more images for this group
+        ],
+      },
+      // Add more image groups as needed
+    ];
+    return (
+      <div className="row g-5">
+        {imageGroups.map((group, index) => (
+          <div key={index} className="gallery-item">
+            <div className="gallery-item-title">
+              <h4>{group.title}</h4>
+            </div>
+            <div className="row g-3">
+              {group.images.map((image, i) => (
+                <div
+                  key={i}
+                  className="col-6 col-md-4 col-lg-3 col-xl-2 gallery-img d-flex justify-content-center align-items-center"
+                  onClick={() => openModal(image)}
+                >
+                  <img
+                    src={image}
+                    alt={`sample image ${i + 1}`}
+                    className="img-fluid"
+                  />
+                </div>
+              ))}
+            </div>
+          </div>
+        ))}
+      </div>
+    );
+  }
 
   const openModal = (imageSrc) => {
     setSelectedImage(imageSrc);
@@ -65,32 +92,7 @@ export default function Gallery() {
           sectiondescription="Our"
           sectiondescriptionbold="Gallery"
         />
-        <div className="container">
-          <div className="row g-5">
-            {imageGroups.map((group, index) => (
-              <div key={index} className="gallery-item">
-                <div className="gallery-item-title">
-                  <h4>{group.title}</h4>
-                </div>
-                <div className="row g-3">
-                  {group.images.map((image, i) => (
-                    <div
-                      key={i}
-                      className="col-6 col-md-4 col-lg-3 col-xl-2 gallery-img d-flex justify-content-center align-items-center"
-                      onClick={() => openModal(image)}
-                    >
-                      <img
-                        src={image}
-                        alt={`sample image ${i + 1}`}
-                        className="img-fluid"
-                      />
-                    </div>
-                  ))}
-                </div>
-              </div>
-            ))}
-          </div>
-        </div>
+        <div className="container">{galleryData()}</div>
       </section>
 
       <Modal
