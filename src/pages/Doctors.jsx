82ca48a7ecebc42e14c3d3a1a31308592af7890b
Shalvin shaji <shalvinshaji283@gmail.com>
2023-12-16 15:26:30 +0530

    doctors page divided into functions

--- src/pages/Doctors.jsx
+++ src/pages/Doctors.jsx
@@ -4,58 +4,77 @@ import Sectionhead from "../components/Sectionhead";
 import "../css/Doctors.css";
 
 export default function Doctors() {
-  // Assuming you have the doctors data from the API
-  const doctorsData = [
-    {
-      id: 1,
-      name: "Dr K. A Chacko",
-      role: "Chief Medical Officer",
-      qualifications: "MBBS, FCAMS",
-      image:
-        "https://yt3.googleusercontent.com/ytc/APkrFKYQtHv2GYhbAQkrTRknWk6YVrDLLZk1YovsUcT0Aw=s900-c-k-c0x00ffffff-no-rj",
-    },
-    {
-      id: 2,
-      name: "Dr K. A Chacko",
-      role: "Chief Medical Officer",
-      qualifications: "MBBS, FCAMS",
-      image:
-        "https://yt3.googleusercontent.com/ytc/APkrFKYQtHv2GYhbAQkrTRknWk6YVrDLLZk1YovsUcT0Aw=s900-c-k-c0x00ffffff-no-rj",
-    },
-    {
-      id: 3,
-      name: "Dr K. A Chacko",
-      role: "Chief Medical Officer",
-      qualifications: "MBBS, FCAMS",
-      image:
-        "https://yt3.googleusercontent.com/ytc/APkrFKYQtHv2GYhbAQkrTRknWk6YVrDLLZk1YovsUcT0Aw=s900-c-k-c0x00ffffff-no-rj",
-    },
-    {
-      id: 4,
-      name: "Dr K. A Chacko",
-      role: "Chief Medical Officer",
-      qualifications: "MBBS, FCAMS",
-      image:
-        "https://yt3.googleusercontent.com/ytc/APkrFKYQtHv2GYhbAQkrTRknWk6YVrDLLZk1YovsUcT0Aw=s900-c-k-c0x00ffffff-no-rj",
-    },
-    {
-      id: 5,
-      name: "Dr K. A Chacko",
-      role: "Chief Medical Officer",
-      qualifications: "MBBS, FCAMS",
-      image:
-        "https://yt3.googleusercontent.com/ytc/APkrFKYQtHv2GYhbAQkrTRknWk6YVrDLLZk1YovsUcT0Aw=s900-c-k-c0x00ffffff-no-rj",
-    },
-    {
-      id: 6,
-      name: "Dr K. A Chacko",
-      role: "Chief Medical Officer",
-      qualifications: "MBBS, FCAMS",
-      image:
-        "https://yt3.googleusercontent.com/ytc/APkrFKYQtHv2GYhbAQkrTRknWk6YVrDLLZk1YovsUcT0Aw=s900-c-k-c0x00ffffff-no-rj",
-    },
-    // Add more doctor objects as needed
-  ];
+  function doctorsData() {
+    var doctorsList = [
+      {
+        id: 1,
+        name: "Dr K. A Chacko",
+        role: "Chief Medical Officer",
+        qualifications: "MBBS, FCAMS",
+        image:
+          "https://yt3.googleusercontent.com/ytc/APkrFKYQtHv2GYhbAQkrTRknWk6YVrDLLZk1YovsUcT0Aw=s900-c-k-c0x00ffffff-no-rj",
+      },
+      {
+        id: 2,
+        name: "Dr K. A Chacko",
+        role: "Chief Medical Officer",
+        qualifications: "MBBS, FCAMS",
+        image:
+          "https://yt3.googleusercontent.com/ytc/APkrFKYQtHv2GYhbAQkrTRknWk6YVrDLLZk1YovsUcT0Aw=s900-c-k-c0x00ffffff-no-rj",
+      },
+      {
+        id: 3,
+        name: "Dr K. A Chacko",
+        role: "Chief Medical Officer",
+        qualifications: "MBBS, FCAMS",
+        image:
+          "https://yt3.googleusercontent.com/ytc/APkrFKYQtHv2GYhbAQkrTRknWk6YVrDLLZk1YovsUcT0Aw=s900-c-k-c0x00ffffff-no-rj",
+      },
+      {
+        id: 4,
+        name: "Dr K. A Chacko",
+        role: "Chief Medical Officer",
+        qualifications: "MBBS, FCAMS",
+        image:
+          "https://yt3.googleusercontent.com/ytc/APkrFKYQtHv2GYhbAQkrTRknWk6YVrDLLZk1YovsUcT0Aw=s900-c-k-c0x00ffffff-no-rj",
+      },
+      {
+        id: 5,
+        name: "Dr K. A Chacko",
+        role: "Chief Medical Officer",
+        qualifications: "MBBS, FCAMS",
+        image:
+          "https://yt3.googleusercontent.com/ytc/APkrFKYQtHv2GYhbAQkrTRknWk6YVrDLLZk1YovsUcT0Aw=s900-c-k-c0x00ffffff-no-rj",
+      },
+      {
+        id: 6,
+        name: "Dr K. A Chacko",
+        role: "Chief Medical Officer",
+        qualifications: "MBBS, FCAMS",
+        image:
+          "https://yt3.googleusercontent.com/ytc/APkrFKYQtHv2GYhbAQkrTRknWk6YVrDLLZk1YovsUcT0Aw=s900-c-k-c0x00ffffff-no-rj",
+      },
+      // Add more doctor objects as needed
+    ];
+    return (
+      <div className="row">
+        {doctorsList.map((doctor) => (
+          <div key={doctor.id} className="col-md-6 col-lg-4 col-xl-3">
+            <div className="member d-flex flex-column align-items-center justify-content-center">
+              <div className="pic">
+                <img src={doctor.image} className="mb-3" alt="" />
+              </div>
+              <div className="member-info text-center">
+                <h4>{doctor.name}</h4>
+                <span>{doctor.qualifications}</span>
+                <span>{doctor.role}</span>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    );
+  }
 
   return (
     <>
@@ -66,24 +85,7 @@ export default function Doctors() {
           sectiondescription="Our"
           sectiondescriptionbold="Doctors"
         />
-        <div className="container">
-          <div className="row">
-            {doctorsData.map((doctor) => (
-              <div key={doctor.id} className="col-md-6 col-lg-4 col-xl-3">
-                <div className="member d-flex flex-column align-items-center justify-content-center">
-                  <div className="pic">
-                    <img src={doctor.image} className="mb-3" alt="" />
-                  </div>
-                  <div className="member-info text-center">
-                    <h4>{doctor.name}</h4>
-                    <span>{doctor.qualifications}</span>
-                    <span>{doctor.role}</span>
-                  </div>
-                </div>
-              </div>
-            ))}
-          </div>
-        </div>
+        <div className="container">{doctorsData()}</div>
       </section>
     </>
   );
