579ab42732faa9c088e6925f40ebd16aa6bd6704
Shalvin shaji <shalvinshaji283@gmail.com>
2023-12-16 15:08:02 +0530

    governing board page divided into functions

--- src/pages/Governingboard.jsx
+++ src/pages/Governingboard.jsx
@@ -4,34 +4,63 @@ import Sectionhead from "../components/Sectionhead";
 import "../css/Governingboard.css";
 
 export default function Governingboard() {
-  const governingBoardMembers = [
-    {
-      id: 1,
-      position: "Patron",
-      name: "Rt Rev Dr. Malayil Sabu Koshy Cherian",
-    },
-    {
-      id: 2,
-      position: "Patron",
-      name: "Rt Rev Dr. Malayil Sabu Koshy Cherian",
-    },
-    {
-      id: 3,
-      position: "Patron",
-      name: "Rt Rev Dr. Malayil Sabu Koshy Cherian",
-    },
-    {
-      id: 4,
-      position: "Patron",
-      name: "Rt Rev Dr. Malayil Sabu Koshy Cherian",
-    },
-    {
-      id: 5,
-      position: "Patron",
-      name: "Rt Rev Dr. Malayil Sabu Koshy Cherian",
-    },
-    // Add more members as needed
-  ];
+  function governingBoardMembersData() {
+    var governingBoardDesc = `The administration is vested with the Rev. George Mathen Medical
+                              Mission Charitable Society which is headed by the Vicar of the
+                              Holy Immanuel CSI Church as the President and the Bishop of the
+                              CSI Madhya Kerala Diocese as the Patron. The health ministry of
+                              the Holy Immanuel CSI Church, Mallappally is carried out through
+                              this society. The society registration number is A 121/77`;
+
+    var governingBoardMembers = [
+      {
+        id: 1,
+        position: "Patron",
+        name: "Rt Rev Dr. Malayil Sabu Koshy Cherian",
+      },
+      {
+        id: 2,
+        position: "Patron",
+        name: "Rt Rev Dr. Malayil Sabu Koshy Cherian",
+      },
+      {
+        id: 3,
+        position: "Patron",
+        name: "Rt Rev Dr. Malayil Sabu Koshy Cherian",
+      },
+      {
+        id: 4,
+        position: "Patron",
+        name: "Rt Rev Dr. Malayil Sabu Koshy Cherian",
+      },
+      {
+        id: 5,
+        position: "Patron",
+        name: "Rt Rev Dr. Malayil Sabu Koshy Cherian",
+      },
+    ];
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <p>{governingBoardDesc}</p>
+          </div>
+        </div>
+        <div className="row my-4">
+          {governingBoardMembers.map((member) => (
+            <div key={member.id} className="col-md-6 col-lg-4 col-xl-3">
+              <div className="member d-flex flex-column align-items-center justify-content-center">
+                <div className="member-info text-center">
+                  <span>{member.position}</span>
+                  <h4>{member.name}</h4>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  }
 
   return (
     <>
@@ -42,32 +71,7 @@ export default function Governingboard() {
           sectiondescription="Our"
           sectiondescriptionbold="Board of directors"
         />
-        <div className="container">
-          <div className="row">
-            <div className="col-12">
-              <p>
-                The administration is vested with the Rev. George Mathen Medical
-                Mission Charitable Society which is headed by the Vicar of the
-                Holy Immanuel CSI Church as the President and the Bishop of the
-                CSI Madhya Kerala Diocese as the Patron. The health ministry of
-                the Holy Immanuel CSI Church, Mallappally is carried out through
-                this society. The society registration number is A 121/77
-              </p>
-            </div>
-          </div>
-          <div className="row my-4">
-            {governingBoardMembers.map((member) => (
-              <div key={member.id} className="col-md-6 col-lg-4 col-xl-3">
-                <div className="member d-flex flex-column align-items-center justify-content-center">
-                  <div className="member-info text-center">
-                    <span>{member.position}</span>
-                    <h4>{member.name}</h4>
-                  </div>
-                </div>
-              </div>
-            ))}
-          </div>
-        </div>
+        {governingBoardMembersData()}
       </section>
     </>
   );
