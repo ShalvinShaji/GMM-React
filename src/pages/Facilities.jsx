9c5e06377684762664f53390c5bf6d2e7809b136
Shalvin shaji <shalvinshaji283@gmail.com>
2023-12-16 15:18:13 +0530

    facilities page divided into functions

--- src/pages/Facilities.jsx
+++ src/pages/Facilities.jsx
@@ -4,33 +4,50 @@ import Sectionhead from "../components/Sectionhead";
 import "../css/Facilities.css";
 
 export default function Facilities() {
-  const facilitiesData = [
-    {
-      id: 1,
-      name: "Casualty",
-      image:
-        "https://www.onlinelogomaker.com/blog/wp-content/uploads/2017/07/medical-logo.jpg",
-    },
-    {
-      id: 2,
-      name: "Casualty",
-      image:
-        "https://www.onlinelogomaker.com/blog/wp-content/uploads/2017/07/medical-logo.jpg",
-    },
-    {
-      id: 3,
-      name: "Casualty",
-      image:
-        "https://www.onlinelogomaker.com/blog/wp-content/uploads/2017/07/medical-logo.jpg",
-    },
-    {
-      id: 4,
-      name: "Casualty",
-      image:
-        "https://www.onlinelogomaker.com/blog/wp-content/uploads/2017/07/medical-logo.jpg",
-    },
-    // Add more facility objects as needed
-  ];
+  function facilitiesData() {
+    const facilitiesList = [
+      {
+        id: 1,
+        name: "Casualty",
+        image:
+          "https://www.onlinelogomaker.com/blog/wp-content/uploads/2017/07/medical-logo.jpg",
+      },
+      {
+        id: 2,
+        name: "Casualty",
+        image:
+          "https://www.onlinelogomaker.com/blog/wp-content/uploads/2017/07/medical-logo.jpg",
+      },
+      {
+        id: 3,
+        name: "Casualty",
+        image:
+          "https://www.onlinelogomaker.com/blog/wp-content/uploads/2017/07/medical-logo.jpg",
+      },
+      {
+        id: 4,
+        name: "Casualty",
+        image:
+          "https://www.onlinelogomaker.com/blog/wp-content/uploads/2017/07/medical-logo.jpg",
+      },
+    ];
+    return (
+      <div className="row">
+        {facilitiesList.map((facility) => (
+          <div className="col-lg-4 col-xl-3 col-md-6" key={facility.id}>
+            <div className="facility">
+              <div className="facility-img">
+                <img src={facility.image} alt={facility.name} />
+              </div>
+              <div className="facility-name">
+                <h3 className="text-uppercase">{facility.name}</h3>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    );
+  }
 
   return (
     <>
@@ -41,22 +58,7 @@ export default function Facilities() {
           sectiondescription="Our"
           sectiondescriptionbold="Facilities"
         />
-        <div className="container">
-          <div className="row">
-            {facilitiesData.map((facility) => (
-              <div className="col-lg-4 col-xl-3 col-md-6" key={facility.id}>
-                <div className="facility">
-                  <div className="facility-img">
-                    <img src={facility.image} alt={facility.name} />
-                  </div>
-                  <div className="facility-name">
-                    <h3 className="text-uppercase">{facility.name}</h3>
-                  </div>
-                </div>
-              </div>
-            ))}
-          </div>
-        </div>
+        <div className="container">{facilitiesData()}</div>
       </section>
     </>
   );
